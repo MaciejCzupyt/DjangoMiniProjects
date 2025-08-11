b4774ee5cf8462843e9ca38a6dfd0e1796bf32ff
Maciej Czupyt <maciej.czupyt@gmail.com>
2025-08-11 13:07:08 +0200

    Add Read, Update, Delete functionalities to ExampleModel

--- project_app/urls.py
+++ project_app/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from . import views
+# from .views import detail_view
 
 from .views import ExampleList, OtherList
 urlpatterns = [
     path('ExampleList', ExampleList.as_view()),
     path('OtherList', OtherList.as_view()),
     path('', views.index, name='index'),
-    path('form', views.create_view),
+    path('ExampleForm', views.create_view),
+    path('<id>', views.detail_view),
+    path('<id>/update', views.update_view),
+    path('<id>/delete', views.delete_view),
     # path('test', views.test, name="test"),
 ]
--- project_app/views.py
+++ project_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from .models import ExampleModel, OtherModel
 from django.views.generic.list import ListView
 from .forms import ExampleForm
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, HttpResponseRedirect
 
 
 def index(request):
@@ -29,3 +29,28 @@ def create_view(request):
     return render(request, "create_view.html", context)
 
 
+def detail_view(request, id):
+    context = {"data": ExampleModel.objects.get(id=id)}
+    return render(request, "detail_view.html", context)
+
+
+def update_view(request, id):
+    obj = get_object_or_404(ExampleModel, id=id)
+    form = ExampleForm(request.POST or None, instance=obj)
+
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect("/" + id)
+
+    context = {"form": form}
+
+    return render(request, "create_view.html", context)
+
+
+def delete_view(request, id):
+    obj = get_object_or_404(ExampleModel, id=id)
+    if request.method == "POST":
+        obj.delete()
+        return HttpResponseRedirect("/ExampleList")
+
+    return render(request, "delete_view.html")
