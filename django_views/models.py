0030e79745d63700d3da445fe1ffae8396c5b57b
Maciej Czupyt <maciej.czupyt@gmail.com>
2025-08-13 17:36:41 +0200

    Add email field with validator to ExampleModel

--- django_views/models.py
+++ django_views/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.core.validators import validate_email
 
 
 class ExampleModel(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
+    email = models.CharField(default='example@example.com', validators=[validate_email])
 
     def __str__(self):
         return self.title
